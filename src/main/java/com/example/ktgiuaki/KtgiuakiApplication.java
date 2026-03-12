392c32b1d8b4ce82c8d1618abe119f1ddb0ea193
Nguyen Long Vu <vunguyenlong707.com>
2026-03-12 09:06:25 +0700

    first commit

---
+++ src/main/java/com/example/ktgiuaki/KtgiuakiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktgiuaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtgiuakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtgiuakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktgiuaki/KtgiuakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktgiuaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtgiuakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
